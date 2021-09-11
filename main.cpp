55461524395f1c35fa648c28f3fd9d241deef0f1
leslie-fang-intel <leslie.fang@intel.com>
2021-09-11 03:05:58 -0700

    modify code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <queue>
 #include <condition_variable>
+#include <unistd.h>
 
 std::mutex Queue_Mutex;
 std::queue<int> Queue;
@@ -22,39 +23,26 @@ int main(int argc, char ** argv){
     std::vector<std::thread> thread_pool;
     const auto thread_id = 0;
     thread_pool.emplace_back([&, thread_id]() {
-        bool ternamal = false;
-        while(true)
         {
-            {
-                {
-                    std::unique_lock<std::mutex> lock(Queue_Mutex);
-                    ++initialized;
-                    worker_main_cv.notify_one();
-                    // NOLINTNEXTLINE(bugprone-infinite-loop)
-                    while (!start) {
-                        main_worker_cv.wait(lock);
-                    }
-                }
-                std::cout<<"Sub thead finish init"<<std::endl;
-                while(true){
-                    if(!Queue.empty()){
-                        timestamp2 = std::chrono::high_resolution_clock::now();
-                        int data = Queue.front();
-                        std::cout<<"sub thread get data: "<<data<<std::endl;
-                        if(data == 100){
-                            ternamal = true;
-                        }
-                        Queue.pop();
-                        if(ternamal){
-                            break;
-                        }
-                    }
-                }
-                if(ternamal){
+            std::unique_lock<std::mutex> lock(Queue_Mutex);
+            ++initialized;
+            worker_main_cv.notify_one();
+            // NOLINTNEXTLINE(bugprone-infinite-loop)
+            while (!start) {
+                main_worker_cv.wait(lock);
+            }
+        }
+        //std::cout<<"Sub thead finish init"<<std::endl;
+        while(true){
+            if (!Queue.empty()) {
+                timestamp2 = std::chrono::high_resolution_clock::now();
+                int data = Queue.front();
+                //std::cout<<"sub thread get data: "<<data<<std::endl;
+                Queue.pop();
+                if(data == 100){
                     break;
                 }
             }
-            continue;
         }
     });
 
@@ -66,18 +54,19 @@ int main(int argc, char ** argv){
     }
     start = true;
     main_worker_cv.notify_all();
-    std::cout<<"Main thead finish init"<<std::endl;
+    //std::cout<<"Main thead finish init"<<std::endl;
+    usleep(1000);
 
     timestamp1 = std::chrono::high_resolution_clock::now();
     Queue.push(100);
-    //worker_main_cv.notify_one();
 
     for (auto& t : thread_pool) {
         t.join();
     }
     float submit_time_us = std::chrono::duration_cast<std::chrono::nanoseconds>(
                             timestamp2 - timestamp1).count() / 1000.0;
-    std::cout<<"submit_time_us is: "<<submit_time_us<<std::endl;
+    std::cout<<"submit_time_us is: "<<submit_time_us<<" us"<<std::endl;
+
     return 0;
 
 }
