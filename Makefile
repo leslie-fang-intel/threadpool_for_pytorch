072c96176dd6217a0863c38908fabb7129d23624
leslie-fang-intel <leslie.fang@intel.com>
2021-09-12 11:36:22 +0800

    enable threadpool base line

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 LDFLAGS=-pthread
 HEADER=
-main: main.o threadpool.o
-	g++ main.o threadpool.o -o main $(LDFLAGS) $(HEADER)
+main: main.o
+	g++ main.o -o main $(LDFLAGS) $(HEADER)
 main.o: main.cpp
 	g++ -c main.cpp -o main.o $(LDFLAGS) $(HEADER)
-threadpool.o: threadpool.cpp
-	g++ -c threadpool.cpp -o threadpool.o $(LDFLAGS) $(HEADER)
 clean:
 	rm -rf main *.o
--- main.cpp
+++ main.cpp
@@ -6,84 +6,94 @@
 #include <queue>
 #include <condition_variable>
 #include <unistd.h>
-#include "threadpool.hpp"
+#include "ThreadPool.h"
 
-std::mutex Queue_Mutex;
-std::queue<int> Queue;
+// std::mutex Queue_Mutex;
+// std::queue<int> Queue;
 
-std::condition_variable worker_main_cv;
-std::condition_variable main_worker_cv;
+// std::condition_variable worker_main_cv;
+// std::condition_variable main_worker_cv;
 
-auto timestamp1 = std::chrono::high_resolution_clock::now();
-auto timestamp2 = std::chrono::high_resolution_clock::now();
+// auto timestamp1 = std::chrono::high_resolution_clock::now();
+// auto timestamp2 = std::chrono::high_resolution_clock::now();
 
-bool start = false;
-int initialized = 0;
+// bool start = false;
+// int initialized = 0;
 
-void test2(){
-    std::cout<<"Hello World"<<std::endl;
-}
-
-std::vector<std::thread> thread_pool;
-
-template <class F>
-void run(const F &f) {
-    thread_pool.emplace_back([&](){
-        f();
-    });
-}
+// int myfunc(int i)
+// {
+//     std::cout<<"Hello World"<<std::endl;
+//     return i*i;
+// }
 
 int main(int argc, char ** argv){
-    //test2();
-    run([&](){std::cout<<"hello world"<<std::endl;});
+    //typedef int (*FunType)(int);
+    
+    ThreadPool pool(1);
+    std::vector< std::future<int> > results;
 
-
-    const auto thread_id = 0;
-    thread_pool.emplace_back([&, thread_id]() {
-        {
-            std::unique_lock<std::mutex> lock(Queue_Mutex);
-            ++initialized;
-            worker_main_cv.notify_one();
-            // NOLINTNEXTLINE(bugprone-infinite-loop)
-            while (!start) {
-                main_worker_cv.wait(lock);
-            }
-        }
-        //std::cout<<"Sub thead finish init"<<std::endl;
-        while(true){
-            if (!Queue.empty()) {
-                timestamp2 = std::chrono::high_resolution_clock::now();
-                int data = Queue.front();
-                //std::cout<<"sub thread get data: "<<data<<std::endl;
-                Queue.pop();
-                if(data == 100){
-                    break;
-                }
-            }
-        }
-    });
-
-    {
-        std::unique_lock<std::mutex> lock(Queue_Mutex);
-        while (initialized != 1) {
-            worker_main_cv.wait(lock);
-        }
+    for(int i = 0; i < 1; ++i) {
+        results.emplace_back(
+            pool.enqueue([i] {
+                return i*i;
+            })
+        );
     }
-    start = true;
-    main_worker_cv.notify_all();
-    //std::cout<<"Main thead finish init"<<std::endl;
-    usleep(1000);
 
-    timestamp1 = std::chrono::high_resolution_clock::now();
-    Queue.push(100);
-
-    for (auto& t : thread_pool) {
-        t.join();
-    }
-    float submit_time_us = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                            timestamp2 - timestamp1).count() / 1000.0;
-    std::cout<<"submit_time_us is: "<<submit_time_us<<" us"<<std::endl;
-
+    for(auto && result: results)
+        std::cout << result.get() << ' '<< std::endl;
     return 0;
+
+    // std::vector<std::thread> thread_pool;
+    // // run([&](){std::cout<<"hello world"<<std::endl;});
+
+
+    // const auto thread_id = 0;
+    // thread_pool.emplace_back([&, thread_id]() {
+    //     {
+    //         std::unique_lock<std::mutex> lock(Queue_Mutex);
+    //         ++initialized;
+    //         worker_main_cv.notify_one();
+    //         // NOLINTNEXTLINE(bugprone-infinite-loop)
+    //         while (!start) {
+    //             main_worker_cv.wait(lock);
+    //         }
+    //     }
+    //     //std::cout<<"Sub thead finish init"<<std::endl;
+    //     while(true){
+    //         if (!Queue.empty()) {
+    //             timestamp2 = std::chrono::high_resolution_clock::now();
+    //             int data = Queue.front();
+    //             //std::cout<<"sub thread get data: "<<data<<std::endl;
+    //             Queue.pop();
+    //             if(data == 100){
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // });
+
+    // {
+    //     std::unique_lock<std::mutex> lock(Queue_Mutex);
+    //     while (initialized != 1) {
+    //         worker_main_cv.wait(lock);
+    //     }
+    // }
+    // start = true;
+    // main_worker_cv.notify_all();
+    // //std::cout<<"Main thead finish init"<<std::endl;
+    // usleep(1000);
+
+    // timestamp1 = std::chrono::high_resolution_clock::now();
+    // Queue.push(100);
+
+    // for (auto& t : thread_pool) {
+    //     t.join();
+    // }
+    // float submit_time_us = std::chrono::duration_cast<std::chrono::nanoseconds>(
+    //                         timestamp2 - timestamp1).count() / 1000.0;
+    // std::cout<<"submit_time_us is: "<<submit_time_us<<" us"<<std::endl;
+
+    // return 0;
 
 }
